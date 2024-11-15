dce67b2d84bd352e9120ffe520110879c2e9f8b5
Coenicorn <coenicorn>
2024-11-15 18:56:17 +0100

    I did things :)))

---
+++ TI/RFID-platformio/src/pins.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+    KIJK OF DEZE PINS OP JOUW EIGEN BORD KLOPPEN!!!!!
+*/
+
+#define RST_PIN 22		// De pin die om de NFC-reader/writer te hard resetten
+#define SS_PIN 5		// De pin die aangesloten is op de SS van de NFC-reader/writer
+
+#define GREEN_LED_PIN 25 // De pin voor het groene led lampje
+#define RED_LED_PIN 26	// De pin voor het rode led lampje
+// used to signal you can hold a card in front
+#define BLUE_LED_PIN 33
+
+#define PIN_TOOL_WIPE 34
+#define PIN_TOOL_NEW_CORRECT_TOKEN 35
+#define PIN_TOOL_TEMP000 32
