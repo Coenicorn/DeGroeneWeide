280b1b073aebe06ddf7489f9caff3fc0f0cd3142
coenicorn <coenicorn.blauwzee@gmail.com>
2024-11-30 17:29:19 +0100

    call correct config and import api server file

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,17 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import { info_log } from './util.js';
-import "./api.js";
-
 import config from './config.js';
+import { verifyCorrectConfiguration } from './config.js';
+verifyCorrectConfiguration();
+
+// start the api server first
+// server file has global code so wait for that code to finish running
+await (async ()=>{
+    info_log("importing api.js...");
+    await import("./api.js");
+    info_log("api.js finished importing");
+})();
 
 // exposed to public
 const app = express();
