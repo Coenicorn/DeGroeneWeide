b340b4b9b4048b5fa32421aefed1ff51c6e757e4
Coenicorn <coenicorn>
2024-12-02 14:44:33 +0100

    response msg util & route from app util

--- backend/util.js
+++ backend/util.js
@@ -21,4 +21,42 @@ export function hastoAcceptJson(req, res, next) {
         res.status(400).send("'Accept' headers must be application/json");
         return;
     }
+    next();
+}
+
+export function resfailwithstatus(res, statusCode, statusMsg, additionalObject) {
+    let ret = {};
+
+    ret.status = statusMsg;
+
+    if (additionalObject !== undefined) ret.additional = additionalObject;
+
+    res.status(statusCode).json(ret);
+}
+
+function routeFromRegexp(regexpStr) {
+    let str = "";
+    if (typeof(regexpStr) === "string") str = regexpStr;
+    else str = regexpStr.toString();
+    str = str.slice(4);
+    str = str.split("\\")[0];
+    if (str.startsWith("?")) return "";
+    return str;
+}
+
+function routesFromStack(stack) {
+    let t = [];
+    for (let r of stack) {
+        if (r.handle.stack) {
+            t.push(...routesFromStack(r.handle.stack));
+        } else {
+            let str = routeFromRegexp(r.regexp);
+            if (str.length !== 0) t.push(str);
+        }
+    }
+    return t;
+}
+
+export function routesFromApp(app) {
+    return routesFromStack(app._router.stack);
 }
