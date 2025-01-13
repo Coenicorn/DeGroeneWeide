317099c6e87496518f8e8debe2cd1a1c2911a8e8
chevyje <jurre.blankers@gmail.com>
2025-01-13 18:08:14 +0100

    bookings api

--- Admin App/DeGroeneWeide/DeGroeneWeide/ApiCalls/BookingApi.cs
+++ Admin App/DeGroeneWeide/DeGroeneWeide/ApiCalls/BookingApi.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using DeGroeneWeide.Objects;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DeGroeneWeide.ApiCalls
 {
     internal class BookingApi
     {
+        public static HttpClient client = new();
+        public static async Task GetCards()
+        {
+            if (!client.DefaultRequestHeaders.Contains("Accept"))
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            }
+
+            try
+            {
+                HttpResponseMessage result = await client.GetAsync(MainForm._settings.URL + "/booking/getallbookings");
+                result.EnsureSuccessStatusCode();
+
+                string json = await result.Content.ReadAsStringAsync();
+                Debug.WriteLine(json);
+
+                //Cards = JsonSerializer.Deserialize<List<Card>>(json);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
     }
 }
