857d28e09bb0d40a0f32d34ec4a642403932d376
coenicorn <coenicorn.blauwzee@gmail.com>
2025-01-22 16:44:49 +0100

    add isPublicRoute function

--- backend/apikey.js
+++ backend/apikey.js
@@ -2,15 +2,22 @@ import config from "./config.js";
 
 // these routes don't get checked for an API key
 const publicRoutes = [
-    "insertCustomer",
+    "/cards/getAllCards",
     "insertBooking"
 ];
 
+export function isPublicRoute(routeName) {
+    return publicRoutes.includes(routeName);
+}
+
 // middleware that checks if the request has access to the route
 export function onlyAdminPanel(req, res, next) {
-    const apiKey = req.header("x-api-key");
+    if (isPublicRoute(req.url)) {
+        next();
+        return;
+    }
 
-    console.log(req);
+    const apiKey = req.header("x-api-key");
 
     if (apiKey !== config.keyAdminPanel) {
         return res.status(403).send("Invalid API key");
