cba5a5b8f6d5b625d11b9d004ec83363f3e5b7cb
coenicorn <coenicorn.blauwzee@gmail.com>
2025-01-22 16:48:06 +0100

    fix file url

--- backend/docgen/doc.js
+++ backend/docgen/doc.js
@@ -147,8 +147,8 @@ export class APIDocGenerator extends DocGenerator {
 
     _toIsPublicString(yes) {
         let str;
-        if (yes) str = "![img_public](https://github.com/Coenicorn/DeGroeneWeide/tree/admin-panel-api-key/backend/docgen/public.png)";
-        else str = "![img_private](https://github.com/Coenicorn/DeGroeneWeide/tree/admin-panel-api-key/backend/docgen/private.png)";
+        if (yes) str = "![img_public](https://github.com/Coenicorn/DeGroeneWeide/tree/main/backend/docgen/public.png)";
+        else str = "![img_private](https://github.com/Coenicorn/DeGroeneWeide/tree/main/backend/docgen/private.png)";
         return str;
     }
 
