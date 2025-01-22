8a773c7248010545b3279529083e489a3f9deace
coenicorn <coenicorn.blauwzee@gmail.com>
2025-01-22 14:21:38 +0100

    c

--- backend/db.js
+++ backend/db.js
@@ -15,9 +15,9 @@ import { abort } from 'process';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.resolve(__dirname, 'data.db');
-const db = new Database(dbPath);
-db.pragma('journal_mode = WAL');
+// const dbPath = path.resolve(__dirname, 'data.db');
+// const db = new Database(dbPath);
+// db.pragma('journal_mode = WAL');
 
 /**
  * Queries the database
