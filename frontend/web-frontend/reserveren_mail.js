80645bf91d596c97aa39b70b96935bfaae442c9c
Aangepast <tobiaslbeumer@gmail.com>
2025-01-22 09:58:39 +0100

    progress

--- frontend/web-frontend/reserveren_mail.js
+++ frontend/web-frontend/reserveren_mail.js
@@ -15,17 +15,36 @@ function sendMail() {
     let checked_aankomsttijd = "";
     let checked_notities = "";
 
-    if(aankomsttijd.value == "") {
+    if(aankomsttijd.value === "") {
         checked_aankomsttijd = "onbekend";
     } else {
         checked_aankomsttijd = aankomsttijd.value;
     }
 
-    if(notities.value == ""){
+    if(notities.value === ""){
         checked_notities = "Er zijn geen notities mee gegeven.";
     } else {
         checked_notities = notities.value;
     }
     location.href=`mailto:shocomellow.boerbert@gmail.com?subject=Reservatie aanvraag camping de Groene Weide&body=Beste Boer Bert, %0D%0A %0D%0A  Ik zou graag bij u willen reserveren op de camping, hier zijn mijn gegevens: %0D%0A %0D%0A Voornaam: ${voornaam.value} %0D%0A Achternaam: ${tussenvoegsel.value} ${achternaam.value} %0D%0A E-mailadres: ${email.value} %0D%0A Telefoonnummer: ${land_code.value} ${telefoonnummer.value} %0D%0A Van ${begin_datum.value} tot ${eind_datum.value} %0D%0A Verwachte aankomsttijd: ${checked_aankomsttijd} %0D%0A Aantal gasten: ${aantal_gasten.value} %0D%0A Type accommodatie: ${accommodatie.value} %0D%0A Notities: ${checked_notities} %0D%0A %0D%0A Fijne dag nog!`
+
+    fetch("http://localhost:3001/api/customer/instertCustomer", {
+        method: "POST",
+        body: JSON.stringify({
+            firstName: voornaam.value,
+            lastName: tussenvoegsel.value + " " + achternaam.value,
+            mailAddress: email.value,
+            phoneNumber: land_code.value + telefoonnummer.value,
+            blacklisted: false,
+            birthDate: new Date(),
+            maySave: true
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+
+    }).then(r =>
+    console.log("success customer aangemaakt"));
+
 }
 
