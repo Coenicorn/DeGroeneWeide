3c3c21f8df87fd632a793bd3670227f6e7a6e19b
TostiTim <tim.vandebeek@student.hu.nl>
2025-01-24 16:50:35 +0100

    Reserveringsformulier checks (#154)
    * checks tot en met datum klaar, naast eind is kleiner dan begin check
    
    * de rest van de checks

--- frontend/web-frontend/reserveren_confirmation.js
+++
@@ -1,7 +0,0 @@
-const confirmButton = document.getElementById("confirm-btn");
-const confirmationContainer = document.getElementById("confirm-container");
-
-confirmButton.addEventListener("click", async () => {
-    confirmationContainer.classList.toggle("hidden");
-});
-
