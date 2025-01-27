4536e5ef799562703b0326bf3323a4d119b9e8a2
chevyje <jurre.blankers@gmail.com>
2025-01-27 15:10:42 +0100

    changes

---
+++ Admin App/DeGroeneWeide/DeGroeneWeide/ApiCalls/AddHeaders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace DeGroeneWeide.ApiCalls
+{
+    public static class AddHeaders
+    {
+        public static async Task<HttpClient> AddHeadersToClient(HttpClient client)
+        {
+            if (!client.DefaultRequestHeaders.Contains("Accept"))
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                return client;
+            }
+
+            if (!client.DefaultRequestHeaders.Contains("x-api-key"))
+            {
+                client.DefaultRequestHeaders.Add("x-api-key", MainForm._settings.KEY);
+                return client;
+            }
+
+            await Task.CompletedTask;
+            return client;
+        }
+    }
+}
