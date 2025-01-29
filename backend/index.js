cafcaa0464d04d5e0c009285379561be0895081d
coenicorn <coenicorn.blauwzee@gmail.com>
2025-01-29 23:45:35 +0100

    f

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.set('trust proxy', 1)
 if (config.enableRateLimiting != "0") {
 app.use(rateLimit({
     windowMs: 5 * 60 * 1000,
-    limit: 5,
+    limit: 100,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
     skip: (req) => {
